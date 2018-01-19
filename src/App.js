b80fe14c1499429f364166e3e68efd88942ae64e
Nikolai Asparuhov <nasparuhov@santech.fr>
2018-01-19 11:55:18 +0200

    fix webpack config file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { SearchBar } from './components/search-bar/search-bar'
 import logo from './logo.svg';
-import './App.scss';
+import styles from './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1  className = "appName">Outdoors-Sports</h1>
-        <SearchBar className="searchBar"/>
+      <div className={styles.App}>
+        <h1  className = {styles.appName}>Outdoors-Sports</h1>
+        <SearchBar className={styles.searchBar}/>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,72 @@
 const path = require('path');
 const publicPath = path.resolve(__dirname, 'public');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const {getIfUtils, removeEmpty} = require('webpack-config-utils');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    path: publicPath,
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', 'json']
-  },
-  module: {
-    rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
-          plugins: ['transform-class-properties']
-        }
-      },
-      {
-        test: /\.css$/,
-        loaders: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [{
-          loader: "style-loader"
-        }, {
-          loader: "css-loader",
-        }, {
-          loader: "sass-loader",
-          options: {
-            sourceMap: true
+module.exports = env => {
+  const {ifProd, ifNotProd} = getIfUtils(env);
+
+  return {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: publicPath,
+      filename: 'bundle.js'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', 'json']
+    },
+    module: {
+      rules: [{
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'react'],
+            plugins: ['transform-class-properties']
           }
-        }]
-      },
-      {
-        test: /\.svg$/,
-        loader: 'url-loader'
-      }
-    ]
-  },
-  plugins: [],
-  devServer: {
-    contentBase: publicPath,
-    noInfo: true
-  },
-  devtool: 'source-map'
+        },
+        {
+          test: /\.css$/,
+          loaders: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            // Minified version of loaders set-up:
+            use: ifProd('css-loader?modules&importLoader=2&sourceMap&camelCase&localIdentName=[hash:base64:5]!sass-loader?sourceMap=true',
+                        'css-loader?modules&importLoader=2&sourceMap&camelCase&localIdentName=[local]!sass-loader?sourceMap=true')
+          }))
+        },
+        {
+          test: /\.svg$/,
+          loader: 'url-loader'
+        }
+      ]
+    },
+    plugins: removeEmpty([
+      new ProgressBarPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new ExtractTextPlugin({
+        filename: ifProd('[name].[chunkhash].css', '[name].css'),
+        disable: false,
+        allChunks: true
+      }),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        inject: 'body',
+      })
+    ]),
+    devServer: {
+      contentBase: publicPath,
+      noInfo: true
+    },
+    devtool: 'source-map'
+  }
 };

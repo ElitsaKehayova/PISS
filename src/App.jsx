8612a35c68342cd3c2bae19f3f7258c981100ae9
Elitsa Kehayova <elica.kahaiova@gmail.com>
2018-01-21 18:15:17 +0200

    Add new card view

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { SearchBar } from './components/search-bar/SearchBar';
 import './App.scss';
+import Card from './components/card/Card';
 
 class App extends Component {
+
   render() {
     return (
       <div className='App'>
         <h1  className='App-name'>Outdoors Sports</h1>
         <SearchBar className='search-bar'/>
+        <Card data = {this.state}/>
       </div>
     );
   }
